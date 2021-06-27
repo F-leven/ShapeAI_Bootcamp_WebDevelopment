eb57ec298592f74c7319d968be047afa542a76d7
F-leven <84837130+F-leven@users.noreply.github.com>
2021-06-27 13:58:52 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Full-Stack Web Dev Training and Internship</h1>
+      <p>
+        The goal of this Training and Internship Program is to equip interns
+        with the unique skills they need to build database-backed APIs and web
+        applications with the proper industry level front-end as well as scale
+        up the deployment for the millions.
+      </p>
+    </div>    
+);
+}
+   
+export default Info;
